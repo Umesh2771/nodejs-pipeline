95bc2ce1c1ae3484ab00e11bbd19aaaf2219b725
Edy Silva <edigleyssonsilva@gmail.com>
2024-10-27 11:32:04 -0300

    test: test error annotation when requiring an es module

---
+++ test/parallel/test-require-esm-with-no-experimental-require-module.mjs
@@ -0,0 +1,21 @@
+import '../common/index.mjs';
+import { test } from 'node:test';
+import * as fixtures from '../common/fixtures.mjs';
+import { spawnSync } from 'node:child_process';
+import assert from 'node:assert';
+
+test('correctly reports errors when trying to import an ESM module', () => {
+  // The following regex matches the error message that is expected to be thrown
+  //
+  // package-type-module/require-esm-error-annotation/index.cjs:1
+  // const app = require('./app');
+
+  const matchRegex = /package-type-module\/require-esm-error-annotation\/index\.cjs:1[\r?\n]const app = require\('\.\/app'\)/;
+  const fixture = fixtures.path('es-modules/package-type-module/require-esm-error-annotation/index.cjs');
+  const args = ['--no-experimental-require-module', fixture];
+
+  const result = spawnSync(process.execPath, args);
+
+  assert.strictEqual(result.status, 1);
+  assert(result.stderr.toString().match(matchRegex));
+});
